6b142b1328833e8490ef433f720cc688e1532466
Avicenna <sinatureli@gmail.com>
2021-12-13 21:43:23 +0000

    day 14 solution

--- template/problem1.py
+++ template/problem1.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def solve_problem1(input_path):
+input_path = ''
+with open(input_path,'r') as fp:
     
-    with open(input_path,'r') as fp:
-        
-        lines = fp.readlines()
-        
+    lines = fp.readlines()
+    
     
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     
     #solve_problem1('./test_input.txt')
     #solve_problem1('./input1.txt')
